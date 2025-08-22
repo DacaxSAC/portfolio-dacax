e2b5832cfeab9bffadf1b6a70a7116acbb6d148e
Nahu <nahumrmercedes@gmail.com>
2025-08-22 00:11:56 -0500

    Se agrego una estructura base

--- portafolio/src/App.jsx
+++ portafolio/src/App.jsx
@@ -4,32 +4,37 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <header>
+        <nav>
+          <div>Dacax</div>
+          <ul>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Servicios</a></li>
+            <li><a href="#">Proyectos</a></li>
+          </ul>
+          <a href="#">Contáctanos</a>
+        </nav>
+      </header>
+
+      <section>
+        <div>
+          <img src="" alt="Logo Dacax" />
+        </div>
+        <h1>Impulsando la Innovación</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Transformamos ideas en realidad con tecnología de vanguardia y un enfoque centrado en el usuario
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <a href="#">Descubre nuestros servicios</a>
+      </section>
+
+      <section className="soluciones">
+        <h2>Nuestras Soluciones</h2>
+        <p>Ofrecemos servicios diseñados para llevar tu negocio al siguiente nivel digital</p>
+      </section>
     </>
-  )
+  );
 }
 
 export default App
